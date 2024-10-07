6238abffbc145b3ac2026ba4a769b7ac2f6c4c9a
mohamed-ashraf <mohamef.ashraf877@gmail.com>
2024-10-07 07:02:15 +0300

    s

--- z/src/assist/footer/footer.js
+++ z/src/assist/footer/footer.js
@@ -6,13 +6,14 @@ import { FaPhoneAlt } from "react-icons/fa";
 
 function Footer() {
    return (
-<>
+<>git status
+
     <div className='foot'>
     <ul className="frut">
        <li><a href='https://www.facebook.com/clark.maged.9?locale=ar_AR' target='blank'><FaFacebook /> FaceBook</a></li> 
        <li><a href='https://www.instagram.com/dr.clark_maged/' target='blank'><FaInstagram/> Instagram</a></li> 
        <li><a href='https://www.gmail.com/clark.maged.9?locale=ar_AR' target='blank'> <CgMail/> Gmail</a></li> 
-       <li><spain className="phoneN" href='1' target='blank'> <FaPhoneAlt/> 01002922153</spain></li> 
+       <li><spain className="phoneN"> <FaPhoneAlt/> 01002922153</spain></li> 
     </ul>
     </div>
 </>    
